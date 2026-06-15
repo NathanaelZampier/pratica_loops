fa9373804c9a34d21ce0104b63430f694a9dc143
pier-ex <nathanaelrog@hotmail.com>
2026-06-14 21:36:12 -0300

    study: pratica de loops

--- pratica_loop/main.js
+++ pratica_loop/main.js
@@ -21,73 +21,214 @@
 //     console.log(numero)
 // } while (numero < 10)
 
-for(let i = 1; i <= 10; i ++) {
-    console.log(i)
-}
+// for(let i = 1; i <= 10; i ++) {
+//     console.log(i)
+// }
 
 
-for(let i = 20; i > 0; i--) {
-    console.log(i)
-}
+// for(let i = 20; i > 0; i--) {
+//     console.log(i)
+// }
 
-for(let i = 0; i < 20; i++) {
-    console.log(i)
-    i++
-}
+// for(let i = 0; i < 20; i++) {
+//     console.log(i)
+//     i++
+// }
 
-for(let i = 0; i < 20; i++) {
-    i++
-    console.log(i)
-}
+// for(let i = 0; i < 20; i++) {
+//     i++
+//     console.log(i)
+// }
 
-let soma = 0
+// let soma = 0
 
-for(let i = 0; i < 11; i ++) {
-    soma += i
-    console.log
-}
-
-
-const jogos = [
-    'minecraft',
-    'gta 5',
-    'fifa',
-    'god of war',
-];
+// for(let i = 0; i < 11; i ++) {
+//     soma += i
+//     console.log
+// }
 
 
-for(let dados of jogos) {
-    console.log(dados)
-}
-
-const numeros = [10,20,30,40,50];
-
-let soma1 = 0
-
-for(let dados of numeros) {
-    soma1 += dados 
-}
-
-console.log(soma1)
-
-const numeros1 = [15,42,8,91,27];
-
-for(let i = 0; i < numeros1.length; i ++) {
-    console.log(numeros1[i])
-    console.log(numeros1[3])
-}
+// const jogos = [
+//     'minecraft',
+//     'gta 5',
+//     'fifa',
+//     'god of war',
+// ];
 
 
-const produtos = [
-    {nome: 'mouse', estoque: 5},
-    {nome: 'teclado', estoque: 0},
-    {nome: 'monitor', estoque: 3},
-    {nome: 'headset', estoque: 0}
-];
+// for(let dados of jogos) {
+//     console.log(dados)
+// }
 
-for(let dados of produtos) {
-    if (dados.estoque === 0) {
-        console.log(dados)
-    }
-}
+// const numeros = [10,20,30,40,50];
+
+// let soma1 = 0
+
+// for(let dados of numeros) {
+//     soma1 += dados 
+// }
+
+// console.log(soma1)
+
+// const numeros1 = [15,42,8,91,27];
+
+// for(let i = 0; i < numeros1.length; i ++) {
+//     console.log(numeros1[i])
+//     console.log(numeros1[3])
+// }
+
+
+// const produtos = [
+//     {nome: 'mouse', estoque: 5},
+//     {nome: 'teclado', estoque: 0},
+//     {nome: 'monitor', estoque: 3},
+//     {nome: 'headset', estoque: 0}
+// ];
+
+// for(let dados of produtos) {
+//     if (dados.estoque === 0) {
+//         console.log(dados)
+//     }
+// }
+
+// for(let i = 1; i <= 30; i+= 2) {
+//     console.log(i)
+// }
+
+// let soma2 = 0
+
+// for(let i = 0; i <= 5; i += 1) {
+//     soma2 += i 
+//     console.log(soma2)
+// }
+
+// const frutas = [
+//     'maça',
+//     'banana',
+//     'laranja',
+//     'uva',
+// ];
+
+// for(let dados of frutas) {
+//     console.log(dados)
+// }
+
+
+// const nomes = [
+//     'nathan',
+//     'pedro',
+//     'lucas',
+//     'joão',
+// ];
+
+// let contador = 0
+
+// for(let i = 0; i < nomes.length; i ++) {
+//     contador++
+//     console.log(contador)
+// }
+
+// const numeros = [1,2,3,4,5,6,7,8,9,10]
+
+// for(let dados of numeros) {
+//     if (dados % 2 === 0) {
+//         console.log(dados)
+//     }
+// }
+
+// const numeros2 = [15,42,8,91,27]
+
+// let maior = numeros2[0]
+
+// for(let i = 0; i < numeros2.length; i ++) {
+//     if (numeros2[i] < maior) {
+//         maior = numeros2[i]
+//     }
+// }
+// console.log(maior)
+
+// for(let i = 1; i <=10; i ++) {
+//     console.log(5 * i)
+// }
+
+// for (let i = 20; i > 0; i--) {
+//     if (i % 2 === 0) {
+//         console.log(i)
+//     }
+// }
+
+// let somaPrecos = 0
+
+// const produtos = [
+//     {nome: 'mouse', preco: 100},
+//     {nome: 'teclado', preco: 200},
+//     {nome: 'monitor', preco: 800}
+// ];
+
+// for(let dados of produtos) {
+//     somaPrecos += dados.preco
+// }
+
+// console.log(somaPrecos)
+
+
+// for(let i = 1; i <= 10; i ++) {
+//     console.log(i)
+// }
+
+// for(let i = 1; i <= 10; i++) {
+//     console.log(7 * i)
+// }
+
+// let energia = 100
+
+// while (energia) {
+//     if (energia > 0) {
+//         energia -= 10
+//     }
+//     console.log(energia)
+// }
+
+
+// let numero = 1
+
+// while (numero) {
+//     if (numero < 20) {
+//         console.log(numero)
+//         numero++
+//     }
+// }
+
+// let tentativa = 1;
+
+// do {
+//     if (tentativa <=3 ) {
+//         tentativa++
+//         console.log(tentativa)
+//     }
+
+// } while (tentativa)
+
+
+
+//     let soma = 0 
+
+//     while (soma) {
+//         if (soma <=5) {
+//             soma++
+//             console.log(soma)
+//         }
+        
+//     }
+
+//     let pares = 2
+
+//     do {
+//         if (pares < 20) {
+//             pares += 2
+//             console.log(pares)
+//         }
+
+//     } while (pares)
+
 
---
+++ pratica_loops2/main.js
@@ -0,0 +1,152 @@
+// for(let i = 1; i <= 10; i ++) {
+//     console.log(i)
+// }
+
+// let numero = 16
+// while (numero > 0) {
+//     numero--
+//     console.log(numero)
+// }
+
+// let multiplo5 = 5 
+
+// while (multiplo5 < 50) {
+//     multiplo5 += 5
+//     console.log(multiplo5)
+// }
+
+// let soma = 0
+
+// for(let i = 0; i <= 20; i ++) {
+//     soma += i
+//     console.log(soma)
+// }
+
+// for(let i = 1; i <= 10; i++) {
+//     console.log(9 * i)
+// } 
+
+
+// const cidades = [
+//     'Curitiba',
+//     'Araucária',
+//     'São Paulo',
+//     'Florianópolis'
+// ];
+
+// for(let dados of cidades) {
+//     console.log(dados)
+// }
+
+// const numeros = [10, 75, 30, 100, 45, 60];
+
+// const maior = numeros[0]
+
+// for(let i = 0; i < numeros.length; i++) {
+//     if (numeros[i] >= 50) {
+//         maior[i] = numeros[i]
+//         console.log(maior)
+//     }
+// }
+
+
+// const menor = numeros[0]
+
+// for(let i = 0; i < numeros.length; i ++) {
+//     if (numeros[i] < menor) {
+//         menor = numeros[i]
+//         console.log(menor)
+//     }
+// }
+
+
+// const produtos = [
+//     { nome: 'Mouse', estoque: 5 },
+//     { nome: 'Teclado', estoque: 0 },
+//     { nome: 'Monitor', estoque: 2 },
+//     { nome: 'Headset', estoque: 0 }
+// ];
+
+// for(let dados of produtos) {
+//     if (dados.estoque === 0) {
+//         console.log(dados.nome)
+//     }
+// }
+
+// let tentativa = 0
+
+// do {
+//     console.log(tentativa)
+//     tentativa++
+
+// } while (tentativa > 5)
+
+let soma = 0
+
+for(let i = 2; i <= 10; i+=2) {
+    console.log(soma += i)
+}
+
+
+for(let i = 0; i <= 10; i++) {
+    console.log(i * i)
+}
+
+const jogos = [
+    'Minecraft',
+    'GTA V',
+    'FIFA',
+    'God of War',
+    'CS2'
+];
+
+for(let i = 0; i < jogos.length; i++) {
+    
+}
+
+console.log(jogos)
+
+const numeros = [5, 10, 15, 20, 25];
+
+let soma1 = 0
+
+for(let dados of numeros) {
+    soma1 += dados
+}
+
+console.log(soma1)
+
+const numeros1 = [18, 7, 42, 91, 30];
+
+let maior1 = numeros1[0]
+
+for(let i = 0; i < numeros1.length; i ++) {
+    if (numeros1[i] > maior1)
+        maior1 = numeros1[i]
+}
+
+console.log(maior1)
+
+let menor1 = numeros1[0]
+
+for(let i = 0; i< numeros1.length; i ++) {
+    if (numeros1[i] < menor1) {
+        menor1 = numeros1[i]
+    }
+}
+
+console.log(menor1)
+
+
+const produtos = [
+    { nome: 'Mouse', estoque: 5 },
+    { nome: 'Teclado', estoque: 0 },
+    { nome: 'Monitor', estoque: 2 },
+    { nome: 'Headset', estoque: 1 }
+];
+
+for(let dados of produtos) {
+    if (dados.estoque > 0) {
+        console.log(dados.nome)
+    }
+}
